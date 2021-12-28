eff68838ccde55fd43e84358ce015f2dabbe28be
Jose Luis <joseluismartinez1024@gmail.com>
2021-12-28 14:13:48 -0600

    Crear GUI.

--- Numeros Complejos/Form1.Designer.cs
+++ Numeros Complejos/Form1.Designer.cs
@@ -28,12 +28,295 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtIm2 = new System.Windows.Forms.TextBox();
+            this.txtRe2 = new System.Windows.Forms.TextBox();
+            this.lblResOp = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtIm1 = new System.Windows.Forms.TextBox();
+            this.txtRe1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbnPow = new System.Windows.Forms.Button();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtImg = new System.Windows.Forms.TextBox();
+            this.txtReal = new System.Windows.Forms.TextBox();
+            this.lblResPow = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDivide.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(581, 195);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(103, 44);
+            this.btnDivide.TabIndex = 65;
+            this.btnDivide.Text = "Dividir";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnMultiply.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(472, 195);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(103, 44);
+            this.btnMultiply.TabIndex = 64;
+            this.btnMultiply.Text = "Multiplicar";
+            this.btnMultiply.UseVisualStyleBackColor = false;
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSubtract.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSubtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtract.Location = new System.Drawing.Point(581, 145);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(103, 44);
+            this.btnSubtract.TabIndex = 63;
+            this.btnSubtract.Text = "Restar";
+            this.btnSubtract.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(472, 145);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(103, 44);
+            this.btnAdd.TabIndex = 62;
+            this.btnAdd.Text = "Sumar";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // txtIm2
+            // 
+            this.txtIm2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIm2.Location = new System.Drawing.Point(253, 230);
+            this.txtIm2.Name = "txtIm2";
+            this.txtIm2.Size = new System.Drawing.Size(134, 26);
+            this.txtIm2.TabIndex = 61;
+            // 
+            // txtRe2
+            // 
+            this.txtRe2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRe2.Location = new System.Drawing.Point(47, 229);
+            this.txtRe2.Name = "txtRe2";
+            this.txtRe2.Size = new System.Drawing.Size(100, 26);
+            this.txtRe2.TabIndex = 60;
+            // 
+            // lblResOp
+            // 
+            this.lblResOp.AutoSize = true;
+            this.lblResOp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResOp.ForeColor = System.Drawing.Color.Blue;
+            this.lblResOp.Location = new System.Drawing.Point(8, 281);
+            this.lblResOp.Name = "lblResOp";
+            this.lblResOp.Size = new System.Drawing.Size(99, 24);
+            this.lblResOp.TabIndex = 59;
+            this.lblResOp.Text = "Resultado:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(204, 203);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(238, 24);
+            this.label11.TabIndex = 58;
+            this.label11.Text = "Parte Imaginaria Número 2:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(8, 202);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(190, 24);
+            this.label12.TabIndex = 57;
+            this.label12.Text = "Parte Real Número 2:";
+            // 
+            // txtIm1
+            // 
+            this.txtIm1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIm1.Location = new System.Drawing.Point(253, 169);
+            this.txtIm1.Name = "txtIm1";
+            this.txtIm1.Size = new System.Drawing.Size(134, 26);
+            this.txtIm1.TabIndex = 56;
+            // 
+            // txtRe1
+            // 
+            this.txtRe1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRe1.Location = new System.Drawing.Point(47, 169);
+            this.txtRe1.Name = "txtRe1";
+            this.txtRe1.Size = new System.Drawing.Size(100, 26);
+            this.txtRe1.TabIndex = 55;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(204, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(238, 24);
+            this.label7.TabIndex = 54;
+            this.label7.Text = "Parte Imaginaria Número 1:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(8, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(190, 24);
+            this.label8.TabIndex = 53;
+            this.label8.Text = "Parte Real Número 1:";
+            // 
+            // tbnPow
+            // 
+            this.tbnPow.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbnPow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.tbnPow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbnPow.Location = new System.Drawing.Point(387, 22);
+            this.tbnPow.Name = "tbnPow";
+            this.tbnPow.Size = new System.Drawing.Size(103, 44);
+            this.tbnPow.TabIndex = 52;
+            this.tbnPow.Text = "Elevar";
+            this.tbnPow.UseVisualStyleBackColor = false;
+            // 
+            // txtN
+            // 
+            this.txtN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN.Location = new System.Drawing.Point(273, 36);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 26);
+            this.txtN.TabIndex = 51;
+            // 
+            // txtImg
+            // 
+            this.txtImg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImg.Location = new System.Drawing.Point(124, 36);
+            this.txtImg.Name = "txtImg";
+            this.txtImg.Size = new System.Drawing.Size(100, 26);
+            this.txtImg.TabIndex = 50;
+            // 
+            // txtReal
+            // 
+            this.txtReal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReal.Location = new System.Drawing.Point(11, 36);
+            this.txtReal.Name = "txtReal";
+            this.txtReal.Size = new System.Drawing.Size(100, 26);
+            this.txtReal.TabIndex = 49;
+            // 
+            // lblResPow
+            // 
+            this.lblResPow.AutoSize = true;
+            this.lblResPow.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResPow.ForeColor = System.Drawing.Color.Blue;
+            this.lblResPow.Location = new System.Drawing.Point(5, 87);
+            this.lblResPow.Name = "lblResPow";
+            this.lblResPow.Size = new System.Drawing.Size(99, 24);
+            this.lblResPow.TabIndex = 48;
+            this.lblResPow.Text = "Resultado:";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.Location = new System.Drawing.Point(269, 9);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(88, 24);
+            this.Label3.TabIndex = 47;
+            this.Label3.Text = "Potencia ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 24);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Parte Imaginaria";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 24);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Parte Real";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(689, 324);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtIm2);
+            this.Controls.Add(this.txtRe2);
+            this.Controls.Add(this.lblResOp);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtIm1);
+            this.Controls.Add(this.txtRe1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbnPow);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.txtImg);
+            this.Controls.Add(this.txtReal);
+            this.Controls.Add(this.lblResPow);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Números Complejos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtIm2;
+        private System.Windows.Forms.TextBox txtRe2;
+        private System.Windows.Forms.Label lblResOp;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtIm1;
+        private System.Windows.Forms.TextBox txtRe1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button tbnPow;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.TextBox txtImg;
+        private System.Windows.Forms.TextBox txtReal;
+        private System.Windows.Forms.Label lblResPow;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
